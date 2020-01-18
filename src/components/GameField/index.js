784cfea59a38bda1294645e8bf52f33c5bab9b10
jake-freeman <Jacob.Freeman@vorne.com>
2020-01-17 22:43:13 -0600

    Paddles move!

--- src/components/GameField/index.js
+++ src/components/GameField/index.js
@@ -8,6 +8,8 @@ const PADDLE_WIDTH = 20;
 const BOARD_WIDTH = 1280;
 const BOARD_HEIGHT = 720;
 
+const TICK_DURATION = 10;
+
 const GameFieldView = styled.div`
     background-color: #0d0d0d;
     width: ${BOARD_WIDTH}px;
@@ -21,13 +23,145 @@ export class GameField extends React.Component {
         const startingY = (BOARD_HEIGHT / 2) - (PADDLE_HEIGHT / 2);
 
         this.state = {
-            p1Y: startingY,
-            p2Y: startingY,
+            p1: {
+                y: startingY,
+            },
+            p2: {
+                y: startingY,
+            },
+            ball: {
+                x: 0,
+                y: 0,
+                velocity: {
+                    x: 0,
+                    y: 0,
+                }
+            },
+            input: {
+                arrowUp: false,
+                arrowDown: false,
+            },
         };
+
+        this._lastTick = Date.now();
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this._handleKeyDown, false);
+        document.addEventListener('keyup', this._handleKeyUp, false);
+
+        this.tick();
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this._handleKeyDown, false);
+        document.removeEventListener('keyup', this._handleKeyUp, false);
+    }
+
+    tick = () => {
+        const now = Date.now();
+        const elapsed = now - this._lastTick;
+
+        this._lastTick = now;
+
+        if (elapsed > TICK_DURATION) {
+            this._applyInputsToPaddles(elapsed);
+        }
+
+        requestAnimationFrame(this.tick);
+    }
+
+    _applyInputsToPaddles = (elapsed) => {
+        const { input: { arrowUp, arrowDown } } = this.state;
+
+        if (arrowUp || arrowDown) {
+            let velocity = 0;
+
+            velocity += arrowUp ? -0.5 : 0;
+            velocity += arrowDown ? 0.5 : 0;
+
+            if (velocity !== 0) {
+                this.setState((state) => {
+                    let { p1 } = state;
+
+                    p1.y += (velocity * elapsed);
+
+                    p1.y = Math.max(0, Math.min(p1.y, BOARD_HEIGHT - PADDLE_HEIGHT));
+
+                    return {
+                        p1,
+                    };
+                });
+            }
+        }
+    }
+
+    _handleKeyDown = (event) => {
+        switch (event.key) {
+            case 'ArrowUp':
+                // Up pressed
+                this.setState((state) => {
+                    let { input } = state;
+
+                    return {
+                        input: {
+                            ...input,
+                            arrowUp: true,
+                        }
+                    };
+                });
+                break;
+            case 'ArrowDown':
+                // Down pressed
+                this.setState((state) => {
+                    let { input } = state;
+
+                    return {
+                        input: {
+                            ...input,
+                            arrowDown: true,
+                        }
+                    };
+                });
+                break;
+        }
+
+        event.preventDefault();
+    }
+
+    _handleKeyUp = (event) => {
+        switch (event.key) {
+            case 'ArrowUp':
+                // Up pressed
+                this.setState((state) => {
+                    let { input } = state;
+
+                    return {
+                        input: {
+                            ...input,
+                            arrowUp: false,
+                        }
+                    };
+                });
+                break;
+            case 'ArrowDown':
+                // Down pressed
+                this.setState((state) => {
+                    let { input } = state;
+
+                    return {
+                        input: {
+                            ...input,
+                            arrowDown: false,
+                        }
+                    };
+                });
+                break;
+        }
     }
 
     render() {
-        const { p1Y, p2Y } = this.state;
+        const { p1, p2 } = this.state;
 
         return (
             <GameFieldView>
@@ -35,13 +169,13 @@ export class GameField extends React.Component {
                     height={PADDLE_HEIGHT}
                     width={PADDLE_WIDTH}
                     x={200}
-                    y={p1Y}
+                    y={p1.y}
                 />
                 <Paddle
                     height={PADDLE_HEIGHT}
                     width={PADDLE_WIDTH}
                     x={1080}
-                    y={p2Y}
+                    y={p2.y}
                 />
             </GameFieldView>
         );
